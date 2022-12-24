0affe6c034761f121e0df18113f8015e4bfcd03d
abhayraihal <b20144@students.iitmandi.ac.in>
2022-12-24 13:46:21 +0530

    some modifications

--- src/App.js
+++ src/App.js
@@ -10,8 +10,9 @@ import {
 } from "react-router-dom";
 
 export default class App extends Component {
+  API = "439f2438279f42a2b47bbbae2b53888e";
+  pageSize = 9;
   render() {
-    let API = "439f2438279f42a2b47bbbae2b53888e";
     return (
       <Router>
         <div>
@@ -20,28 +21,28 @@ export default class App extends Component {
             {/* we use key here because if we don't use it, than react will see that News is already opened and mounted, so it will not mount the different page again beause all values are same */}
             {/* so we use a key here for each news component to make them different and force mount*/}
             <Route exact path='/' element={
-              <News key="general" pageSize={9} country="in" category="general" apiKey={API} />
+              <News key="general" pageSize={this.pageSize} country="in" category="general" apiKey={this.API} />
             } />
             <Route exact path='/business' element={
-              <News key="business" pageSize={9} country="in" category="business" apiKey={API} />
+              <News key="business" pageSize={this.pageSize} country="in" category="business" apiKey={this.API} />
             } />
             <Route exact path='/entertainment' element={
-              <News key="entertainment" pageSize={9} country="in" category="entertainment" apiKey={API} />
+              <News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" apiKey={this.API} />
             } />
             <Route exact path='/general' element={
-              <News key="general" pageSize={9} country="in" category="general" apiKey={API} />
+              <News key="general" pageSize={this.pageSize} country="in" category="general" apiKey={this.API} />
             } />
             <Route exact path='/health' element={
-              <News key="health" pageSize={9} country="in" category="health" apiKey={API} />
+              <News key="health" pageSize={this.pageSize} country="in" category="health" apiKey={this.API} />
             } />
             <Route exact path='/science' element={
-              <News key="science" pageSize={9} country="in" category="science" apiKey={API} />
+              <News key="science" pageSize={this.pageSize} country="in" category="science" apiKey={this.API} />
             } />
             <Route exact path='/sports' element={
-              <News key="sports" pageSize={9} country="in" category="sports" apiKey={API} />
+              <News key="sports" pageSize={this.pageSize} country="in" category="sports" apiKey={this.API} />
             } />
             <Route exact path='/technology' element={
-              <News key="technology" pageSize={9} country="in" category="technology" apiKey={API} />
+              <News key="technology" pageSize={this.pageSize} country="in" category="technology" apiKey={this.API} />
             } />
           </Routes>
         </div>
--- src/components/News.js
+++ src/components/News.js
@@ -28,54 +28,44 @@ export class News extends Component {
 
   // async is used to make the function asynchronous and await is used to wait for the data to be fetched from the api
   // in class based components we use componentDidMount to fetch data from the api
-  async componentDidMount() {
+  async updateNews() {
     // fetch data from the url and convert it into json and the update our articles variable which is initialized in constructor setState 
     // await is used to wait for the data to be fetched and then only the next line will be executed
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=1&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     // console.log(parsedData);
     this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false });
   }
+
+  async componentDidMount() {
+    this.updateNews();
+  }
   handlePrevClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles,
-      loading: false
-    });
+    await this.setState({ page: this.state.page - 1 });
+    this.updateNews();
   }
   handleNextClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({
-      page: this.state.page + 1,
-      articles: parsedData.articles,
-      loading: false
-    });
+    await this.setState({ page: this.state.page + 1 });
+    this.updateNews();
   }
 
   render() {
     return (
       <div className='container my-3'>
-        <h2 className='text-center' style={{margin:"30px"}}>NewsBuster - Top Headlines</h2>
-        {this.state.loading && <Spinner/>}
+        <h2 className='text-center' style={{ margin: "30px" }}>NewsBuster - Top Headlines</h2>
+        {this.state.loading && <Spinner />}
         <div className="row">
-          {!this.state.loading &&  this.state.articles.map((element) => {
+          {!this.state.loading && this.state.articles.map((element) => {
             return <div className="col-md-4" key={element.url}>
-              <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
+              <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
             </div>
           })}
         </div>
         <div className="container d-flex justify-content-between">
-          <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
-          <button disabled={this.state.page+1>Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
